7627bcf1b35f8fc881b3a10c8cf6a0dfc153d8e0
Dean Wan <deanwan14@gmail.com>
2024-02-11 21:13:45 +1100

    feat: add yaml parsing

--- handler.go
+++ handler.go
@@ -1,8 +1,16 @@
 package main
 
 import (
+	"log"
 	"net/http"
+
+	"gopkg.in/yaml.v2"
 )
+
+type ParsedYaml struct {
+	Path string `yaml:"path"`
+	URL  string `yaml:"url"`
+}
 
 // MapHandler will return an http.HandlerFunc (which also
 // implements http.Handler) that will attempt to map any
@@ -11,11 +19,12 @@ import (
 // If the path is not provided in the map, then the fallback
 // http.Handler will be called instead.
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
-	// This is the boilerplate wrapping code that returns a http.HandlerFunc function type
+	// This is the boilerplate wrapping code that returns a
+	// http.HandlerFunc function type
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		newUrl, ok := pathsToUrls[r.URL.Path]
+		redirectUrl, ok := pathsToUrls[r.URL.Path]
 		if ok {
-			http.Redirect(w, r, newUrl, http.StatusFound)
+			http.Redirect(w, r, redirectUrl, http.StatusFound)
 		} else {
 			fallback.ServeHTTP(w, r)
 		}
@@ -39,6 +48,37 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 // See MapHandler to create a similar http.HandlerFunc via
 // a mapping of paths to urls.
 func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	// TODO: Implement this...
-	return nil, nil
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Parse YAML
+		// We require a slice return type as otherwise only the
+		// first entry in the YAML is retained
+		var output []ParsedYaml
+
+		err := yaml.Unmarshal(yml, &output)
+		if err != nil {
+			log.Fatalf("cannot unmarshal yaml data: %v", err)
+		}
+
+		// check if path exists
+		pathMap := parseYamlToMap(output)
+		// Attempting to use MapHandler here doesn't appear
+		// to work - there is some weird double redirect
+		// happening under the hood somewhere
+		redirectUrl, ok := pathMap[r.URL.Path]
+		if ok {
+			http.Redirect(w, r, redirectUrl, http.StatusFound)
+		} else {
+			fallback.ServeHTTP(w, r)
+		}
+	}), nil
+}
+
+func parseYamlToMap(parsedYaml []ParsedYaml) map[string]string {
+	var shortenerKeys = make(map[string]string)
+
+	for _, entry := range parsedYaml {
+		shortenerKeys[entry.Path] = entry.URL
+	}
+
+	return shortenerKeys
 }
